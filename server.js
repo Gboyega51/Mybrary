eed3bf2d2e37cfdf599424009e2f7c964f0af170
Gboyega51 <gboyega51@gmail.com>
2021-07-29 20:15:36 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+//view engine setup
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+//importing the index router
+const indexRouter = require("./routes/index");
+
+//connect to mongoodb
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to mongoose"));
+
+//hooking up the route to the server
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000...");
+});
